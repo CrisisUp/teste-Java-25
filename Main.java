18fe85d14400315c337d03b1028646122ca24927
CrisisUp <cristiano.1973.on@gmail.com>
2026-01-19 17:57:56 -0300

    primeiro commit

--- Main.java
+++
@@ -1,4 +0,0 @@
-void main() {
-    IO.println("Olá do Java 25!");
-    IO.println("O sistema está configurado e pronto.");
-}
---
+++ OlaMundo.java
@@ -0,0 +1,8 @@
+public class OlaMundo {
+    public static void main(String[] args) {
+        // Substituímos o IO.println (exclusivo do modo compacto/preview)
+        // pelo padrão System.out.println
+        System.out.println("Olá do Java!");
+        System.out.println("O sistema está configurado e pronto.");
+    }
+}
